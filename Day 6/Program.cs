f12a94162df3b68da38ad99a0f41dc3ef5d05d72
Andy Wang <andy.wang@rpc.co.uk>
2019-12-06 15:58:15 +0000

    Initial Day 6 commit

---
+++ Day 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = File.ReadAllLines("Input.txt");
+            List<string[]> input = processInput(rawInput);
+
+            
+        }
+
+        static List<string[]> processInput(string[] rawInput)
+        {
+            List<string[]> processedInput = new List<string[]>();
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                string[] row = new string[2];
+                row = rawInput[i].Split(')');
+                processedInput.Add(row);
+            }
+
+            return processedInput;
+        }
+    }
+}
