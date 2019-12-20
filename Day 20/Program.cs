16263e448c77fbf1dccb7fd6bbaef39608e64fdc
Andy Wang <andywang.16@gmail.com>
2019-12-20 15:59:17 +0000

    Day 20 project files

---
+++ Day 20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
