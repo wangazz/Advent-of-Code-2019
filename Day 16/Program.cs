96b5bbc6f3d067c6910257eec3343916e1721b26
Andy Wang <andywang.16@gmail.com>
2019-12-22 23:00:53 +0000

    Start of Day 16

---
+++ Day 16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_16
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = File.ReadAllText("Input.txt");
+            int[] inputArray = ParseInput(input);
+            int phases = 100;
+            int elementsToReturn = 8;
+
+            List<char> charList = new List<char>();
+        }
+
+        static int[] ParseInput(string input)
+        {
+            int[] intArray = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                intArray[i] = int.Parse(input[i].ToString());
+            }
+
+            return intArray;
+        }
+    }
+}
