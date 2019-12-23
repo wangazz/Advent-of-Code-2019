ffb5f4cdeb23d20f1b5431705c6ef37c22013460
Andy Wang <andywang.16@gmail.com>
2019-12-23 11:53:40 +0000

    Day 16 Part 2 before optimisations

--- Day 16/Program.cs
+++ Day 16/Program.cs
@@ -10,6 +10,7 @@ namespace Day_16
         static void Main()
         {
             string input = File.ReadAllText("Input.txt");
+            // string input = "03036732577212944063491565474664";
             int[] inputArray = ParseInput(input);
             const int phases = 100;
 
@@ -33,6 +34,36 @@ namespace Day_16
                 resultString += inputArray[element].ToString();
             }
             Console.WriteLine("Part 1 Answer: " + resultString);
+
+
+            // Part 2
+            List<int> input1 = inputArray.ToList();
+            List<int> input2 = new List<int>();
+            for (int i = 0; i < 10000; i++) input2.AddRange(input1);
+            int[] inputArray2 = input2.ToArray();
+
+            // Iterate over each phase
+            for (int phase = 0; phase < phases; phase++)
+            {
+                int[] newArray = new int[inputArray2.Length];
+                for (int element = 0; element < inputArray2.Length; element++)
+                {
+                    int[] pattern = PatternVector(element + 1, inputArray2.Length);
+                    newArray[element] = Math.Abs(ArrayMultiply(inputArray2, pattern).Sum() % 10);
+                }
+                inputArray2 = newArray;
+            }
+
+            // Part 2 Answer
+            string resultString2 = "";
+            int skip = Convert.ToInt32(input.Substring(0, 7));
+            var result2Array = inputArray2.Skip(skip).Take(elementsToReturn);
+            foreach (int element in result2Array) resultString2 += element.ToString();
+            for (int element = 0; element < elementsToReturn; element++)
+            {
+                resultString2 += inputArray2[element].ToString();
+            }
+            Console.WriteLine("Part 2 Answer: " + resultString2);
         }
 
         static int[] ParseInput(string input)
@@ -54,7 +85,7 @@ namespace Day_16
             for (int i = 0; i < n; i++) pattern.Add(1);
             for (int i = 0; i < n; i++) pattern.Add(0);
             for (int i = 0; i < n; i++) pattern.Add(-1);
-            while(pattern.Count <= length)
+            while (pattern.Count <= length)
             {
                 pattern.AddRange(pattern);
             }
