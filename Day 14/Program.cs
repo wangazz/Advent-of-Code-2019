d6a654cf092644ff227f2b0c0afc6189d8ddd1ec
Andy Wang <andywang.16@gmail.com>
2019-12-23 11:53:52 +0000

    Day 14 project files

---
+++ Day 14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] reactions = File.ReadAllLines("Input.txt");
+        }
+    }
+}
