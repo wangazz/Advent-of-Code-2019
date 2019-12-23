e090436c9e4b282b4d0301b8253a60ecff097a2a
Andy Wang <andywang.16@gmail.com>
2019-12-23 10:33:55 +0000

    Solution for Day 16 Part 1

--- Day 16/Program.cs
+++ Day 16/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day_16
 {
@@ -10,10 +11,28 @@ namespace Day_16
         {
             string input = File.ReadAllText("Input.txt");
             int[] inputArray = ParseInput(input);
-            int phases = 100;
-            int elementsToReturn = 8;
+            const int phases = 100;
 
-            List<char> charList = new List<char>();
+            // Iterate over each phase
+            for (int phase = 0; phase < phases; phase++)
+            {
+                int[] newArray = new int[inputArray.Length];
+                for (int element = 0; element < inputArray.Length; element++)
+                {
+                    int[] pattern = PatternVector(element + 1, input.Length);
+                    newArray[element] = Math.Abs(ArrayMultiply(inputArray, pattern).Sum() % 10);
+                }
+                inputArray = newArray;
+            }
+
+            // Part 1 Answer
+            const int elementsToReturn = 8;
+            string resultString = "";
+            for (int element = 0; element < elementsToReturn; element++)
+            {
+                resultString += inputArray[element].ToString();
+            }
+            Console.WriteLine("Part 1 Answer: " + resultString);
         }
 
         static int[] ParseInput(string input)
@@ -26,6 +45,30 @@ namespace Day_16
             }
 
             return intArray;
+        }
+
+        static int[] PatternVector(int n, int length)
+        {
+            List<int> pattern = new List<int>();
+            for (int i = 0; i < n; i++) pattern.Add(0);
+            for (int i = 0; i < n; i++) pattern.Add(1);
+            for (int i = 0; i < n; i++) pattern.Add(0);
+            for (int i = 0; i < n; i++) pattern.Add(-1);
+            while(pattern.Count <= length)
+            {
+                pattern.AddRange(pattern);
+            }
+
+            IEnumerable<int> patternTrim = pattern.Skip(1).Take(length);
+            int[] patternAsArray = patternTrim.ToArray();
+            return patternAsArray;
+        }
+
+        static int[] ArrayMultiply(int[] a, int[] b)
+        {
+            int[] newArray = new int[a.Length];
+            for (int i = 0; i < a.Length; i++) newArray[i] = a[i] * b[i];
+            return newArray;
         }
     }
 }
